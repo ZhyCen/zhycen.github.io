86b23b29bcd9a16228865c999b0cd8c9eb28e150
Zheyang Cen <cenzheyang@gmail.com>
2020-03-21 18:47:35 -0700

    remove submodule

--- pull.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-git pull
-
-# pull all changes for the submodules
-git submodule update --remotee
-
-# if first time then use this command instead 
-# git submodule update --init --recursive
